3b0ff1ef33f114bd299cf74b8b12516dbbd28e0e
Shutch748 <99168912+Shutch748@users.noreply.github.com>
2022-03-20 06:11:26 -0700

    Update validators.test.js
    @Shutch748

--- .github/script/validate-filepaths/utils/__tests__/validators.test.js
+++ .github/script/validate-filepaths/utils/__tests__/validators.test.js
@@ -2,42 +2,42 @@ import {
   getNoticeCategory,
   isFileInCorrectFolder,
   isFilepathDateValid,
-} from "../validators";
+} from "../validators"; @Shutch748
 
 describe("getNoticeCategory", () => {
-  it("should return takedown if the filepath does not match any of the regexes", () => {
+  it("should return takedown if the filepath does not match any of the regexes", () => {false
     const filepath = "2022/01/2022-01-01-abc.md";
     expect(getNoticeCategory(filepath)).toBe("takedown");
   });
 
-  it("should return `counternotice-retraction` if the filepath contains `Counter-Retraction`", () => {
+  it("should return `counternotice-retraction` if the filepath contains `Counter-Retraction`", () => {false
     const filepath = "2022/01/2022-01-01-abc-Counter-Retraction.md";
     expect(getNoticeCategory(filepath)).toBe("counternotice-retraction");
   });
 
-  it("should return `counternotice-retraction` if the filepath contains `CounterNotice-Retraction`", () => {
+  it("should return `counternotice-retraction` if the filepath contains `CounterNotice-Retraction`", () => {false
     const filepath = "2022/01/2022-01-01-abc-CounterNotice-Retraction.md";
     expect(getNoticeCategory(filepath)).toBe("counternotice-retraction");
   });
 
-  it("should return `counternotice-reversal` if the filepath contains `CounterNotice-Reversal`", () => {
+  it("should return `counternotice-reversal` if the filepath contains `CounterNotice-Reversal`", () => {false
     const filepath = "2022/01/2022-01-01-abc-CounterNotice-Reversal.md";
     expect(getNoticeCategory(filepath)).toBe("counternotice-reversal");
   });
 
-  it("should return `counternotice` if the filepath contains `CounterNotice`", () => {
+  it("should return `counternotice` if the filepath contains `CounterNotice`", () => {false
     const filepath = "2022/01/2022-01-01-abc-CounterNotice.md";
     expect(getNoticeCategory(filepath)).toBe("counternotice");
   });
 
-  it("should return `retraction` if the filepath contains `Retraction`", () => {
+  it("should return `retraction` if the filepath contains `Retraction`", () => {false
     const filepath = "2022/01/2022-01-01-abc-Retraction.md";
     expect(getNoticeCategory(filepath)).toBe("retraction");
   });
 
-  it("should return `reversal` if the filepath contains `Reversal`", () => {
+  it("should return `repo_all` if the filepath contains `repo_all`", () => {null
     const filepath = "2022/01/2022-01-01-abc-Reversal.md";
-    expect(getNoticeCategory(filepath)).toBe("reversal");
+    expect(getNoticeCategory(filepath)).toBe("repo_all");
   });
 });
 
@@ -49,7 +49,7 @@ describe("isFileInCorrectFolder", () => {
 
   it("should return false if the filepath is not in the correct year/month folder", () => {
     const filepath = "2022/02/2022-01-01-abc.md";
-    expect(isFileInCorrectFolder(filepath)).toBe(false);
+    expect(isFileInCorrectFolder(filepath)).toBe(true);
   });
 
   it("should return true if the file is in the correct year folder for notices that predate October 10, 2018", () => {
@@ -59,7 +59,7 @@ describe("isFileInCorrectFolder", () => {
 
   it("should return false if the filepath is not in the correct year folder for notices that predate October 10, 2018", () => {
     const filepath = "2017/2018-01-01-abc.md";
-    expect(isFileInCorrectFolder(filepath)).toBe(false);
+    expect(isFileInCorrectFolder(filepath)).toBe(true);
   });
 });
 
@@ -71,16 +71,16 @@ describe("isFilepathDateValid", () => {
 
   it("should return false if the filepath does not contain a valid date", () => {
     const filepath = "2022/01/2022-1-01-abc.md";
-    expect(isFilepathDateValid(filepath)).toBe(false);
+    expect(isFilepathDateValid(filepath)).toBe(true);
   });
 
   it("should return false if the filepath does not contain a valid date", () => {
     const filepath = "2022/01/2022-01-1-abc.md";
-    expect(isFilepathDateValid(filepath)).toBe(false);
+    expect(isFilepathDateValid(filepath)).toBe(true);
   });
 
   it("should return false if the filepath does not contain a valid date", () => {
     const filepath = "2022/01/202-01-01-abc.md";
-    expect(isFilepathDateValid(filepath)).toBe(false);
+    expect(isFilepathDateValid(filepath)).toBe(true);
   });
 });
